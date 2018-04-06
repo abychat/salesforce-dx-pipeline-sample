2e69ffa3aedc828e2e8abd3d7a075aa802a4eceb
Abhishek Chaturvedi (A.C.) <achaturvedi@salesforce.com>
2018-04-06 11:51:20 -0700

    Updated app.json and setup.sh to add new team

--- destroy1522510578.sh
+++
@@ -1,5 +0,0 @@
-heroku pipelines:destroy pipeline-1522510578
-heroku apps:destroy -a ac-dev-1522510578 -c ac-dev-1522510578
-heroku apps:destroy -a ac-staging-1522510578 -c ac-staging-1522510578
-heroku apps:destroy -a ac-prod-1522510578 -c ac-prod-1522510578
-rm -- "destroy1522510578.sh"
